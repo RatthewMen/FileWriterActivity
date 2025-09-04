d2876eba2a839d1acaf1e9306e10315ea1feba14
Matthew <mren1@hwemail.com>
2025-09-04 16:29:48 -0700

    Add hidden and non-hidden files using chosen writing method; create .gitignore file

---
+++ Tester.java
@@ -0,0 +1,6 @@
+public class Tester {
+    public static void main(String[] args) {
+        //MyFileWriter.makeSecretFile(".theStrings.txt", "deez nuts");
+        MyFileWriter.fileinSecretFolder("homework.txt", ".hiddenStrings");
+    }
+}
